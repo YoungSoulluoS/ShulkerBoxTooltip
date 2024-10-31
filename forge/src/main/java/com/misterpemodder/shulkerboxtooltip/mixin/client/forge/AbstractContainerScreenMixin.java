3f403850084a677e33eb6621ec4d2100959bba6b
Yanis Guaye <yguaye44@gmail.com>
2024-10-31 16:51:08 +0100

    Update to Minecraft 1.21.3
    Also re-enable Forge support.

--- forge/src/main/java/com/misterpemodder/shulkerboxtooltip/mixin/client/forge/AbstractContainerScreenMixin.java
+++ forge/src/main/java/com/misterpemodder/shulkerboxtooltip/mixin/client/forge/AbstractContainerScreenMixin.java
@@ -6,6 +6,7 @@ import net.minecraft.client.gui.Font;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
 import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.inventory.tooltip.TooltipComponent;
 import net.minecraft.world.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
@@ -19,18 +20,18 @@ import java.util.Optional;
 @Mixin(AbstractContainerScreen.class)
 @SuppressWarnings("OptionalUsedAsFieldOrParameterType")
 public class AbstractContainerScreenMixin implements ContainerScreenDrawTooltip {
-  @Redirect(at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/GuiGraphics;renderTooltip("
-      + "Lnet/minecraft/client/gui/Font;Ljava/util/List;Ljava/util/Optional;Lnet/minecraft/world/item/ItemStack;"
-      + "II)V"), method = "renderTooltip(Lnet/minecraft/client/gui/GuiGraphics;II)V")
+  @Redirect(at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/GuiGraphics;renderTooltip"
+      + "(Lnet/minecraft/client/gui/Font;Ljava/util/List;Ljava/util/Optional;"
+      + "Lnet/minecraft/world/item/ItemStack;II)V"), method = "renderTooltip(Lnet/minecraft/client/gui/GuiGraphics;II)V")
   private void lockTooltipPosition(GuiGraphics graphics, Font font, List<Component> text,
       Optional<TooltipComponent> data, ItemStack stack, int x, int y) {
     var self = (ContainerScreenLockTooltip) this;
-    self.shulkerboxtooltip$lockTooltipPosition(graphics, font, text, data, stack, x, y);
+    self.shulkerboxtooltip$lockTooltipPosition(graphics, font, text, data, stack, x, y, null);
   }
 
   @Override
-  public void shulkerboxtooltip$drawMouseoverTooltip(@Nonnull GuiGraphics graphics, Font font, List<Component> text,
-      Optional<TooltipComponent> data, ItemStack stack, int x, int y) {
+  public void shulkerboxtooltip$renderTooltip(@Nonnull GuiGraphics graphics, Font font, List<Component> text,
+      Optional<TooltipComponent> data, ItemStack stack, int x, int y, ResourceLocation backgroundTexture) {
     graphics.renderTooltip(font, text, data, stack, x, y);
   }
 }
